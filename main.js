d9dcbdd5c3a135105f0a9495a984e0887b476a60
Diyorbek <diyorbekolimov2000@gmail.com>
2022-08-06 11:30:54 +0500

    initial commit

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 let mas = document.getElementsByClassName("laptop_teyi");
-let rang = document.querySelectorAll('.buy a');
+let rang = document.querySelectorAll(".buy a");
 
-function laptop_teyi1(a){
-    mas[a-1].style.display = "flex";
-    rang[a-1].style.backgroundColor = "#067ad4"
+function laptop_teyi1(a) {
+  mas[a - 1].style.display = "flex";
+  rang[a - 1].style.backgroundColor = "#067ad4";
 }
 
-function laptop_teyi2(a){
-    mas[a-1].style.display = "none";
-    rang[a-1].style.backgroundColor = "#aaa";
+function laptop_teyi2(a) {
+  mas[a - 1].style.display = "none";
+  rang[a - 1].style.backgroundColor = "#aaa";
 }
-
